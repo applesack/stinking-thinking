8ccd635470cbb9d0ddd1e981330929904dae29fd
applesack <2383955734@qq.com>
2023-01-21 00:26:04 +0800

    LRU缓存

---
+++ adt/adt_struct.go
@@ -0,0 +1,41 @@
+package adt
+
+import "fmt"
+
+type DLinkedNode[T any] struct {
+	Value T
+	Prev  *DLinkedNode[T]
+	Next  *DLinkedNode[T]
+}
+
+type Entry[K any, V any] struct {
+	Key   K
+	Value V
+}
+
+func CreateDLinkedNode[T any](value T) *DLinkedNode[T] {
+	return &DLinkedNode[T]{Value: value}
+}
+
+func BindDLinkedNode[T any](prev, next *DLinkedNode[T]) {
+	prev.Next = next
+	next.Prev = prev
+}
+
+func (t *DLinkedNode[T]) Delete() {
+	p, n := t.Prev, t.Next
+	BindDLinkedNode(p, n)
+	t.Prev, t.Next = nil, nil
+}
+
+func (t *DLinkedNode[T]) String() string {
+	return fmt.Sprintf("[v=%+v, p=%b, n=%b]", t.Value, t.Prev, t.Next)
+}
+
+func CreateEntry[K any, V any](key K, value V) *Entry[K, V] {
+	return &Entry[K, V]{Key: key, Value: value}
+}
+
+func (t *Entry[K, V]) String() string {
+	return fmt.Sprintf("[k=%v, v=%v]", t.Key, t.Value)
+}
--- adt/list/linked_list_test.go
+++ adt/list/linked_list_test.go
@@ -7,9 +7,9 @@ import (
 
 func TestLinkedList_AddFirst(t *testing.T) {
 	list := CreateLinkedList[int]()
-	list.AddLast(1)
-	list.AddLast(2)
-	list.AddFirst(3)
+	list.RPush(1)
+	list.RPush(2)
+	list.LPush(3)
 	fmt.Println(list.Size())
 	fmt.Println(list.Values())
 }
@@ -17,14 +17,14 @@ func TestLinkedList_AddFirst(t *testing.T) {
 func TestLinkedList_Get(t *testing.T) {
 	list := CreateLinkedList[int]()
 	for i := 0; i < 10; i++ {
-		list.AddLast(i)
+		list.RPush(i)
 	}
 	for i := -2; i < 12; i++ {
 		value, err := list.Get(i)
 		if err != nil {
 			fmt.Println(fmt.Sprintf("i=%d, value=%s", i, err.Error()))
 		} else {
-			fmt.Println(fmt.Sprintf("i=%d, value=%d", i, value))
+			fmt.Println(fmt.Sprintf("i=%d, value=%d", i, *value))
 		}
 	}
 }
@@ -35,14 +35,14 @@ func TestLinkedList_Remove(t *testing.T) {
 		if err != nil {
 			fmt.Println(err)
 		} else {
-			fmt.Println(fmt.Sprintf("removed %d", value))
+			fmt.Println(fmt.Sprintf("removed %d", *value))
 			fmt.Println(ls.Values())
 		}
 	}
 
 	list := CreateLinkedList[int]()
 	for i := 0; i < 10; i++ {
-		list.AddLast(i)
+		list.RPush(i)
 	}
 	displayRm(list, 3)
 }
@@ -50,9 +50,20 @@ func TestLinkedList_Remove(t *testing.T) {
 func TestLinkedList_RemoveBoth(t *testing.T) {
 	list := CreateLinkedList[int]()
 	for i := 0; i < 10; i++ {
-		list.AddLast(i)
+		list.RPush(i)
 	}
-	_, _ = list.RemoveFirst()
-	_, _ = list.RemoveLast()
+	_, _ = list.LPop()
+	_, _ = list.RPop()
 	fmt.Println(list.Values())
+}
+
+func TestLinkedList_Struct(t *testing.T) {
+	list := CreateLinkedList[testStruct]()
+	has, s := list.LGet()
+	fmt.Println(has, s)
+}
+
+type testStruct struct {
+	name string
+	age  int
 }
